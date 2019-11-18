d750e4f03d01fc24d3cd47ac6eef276ab8dd67bb
kppomzh <gosiple@126.com>
2019-11-18 14:52:17 +0800

    自动测试程序修复

--- src/test/java/function/ReaderDemo.java
+++ src/test/java/function/ReaderDemo.java
@@ -1,29 +1,34 @@
 package function;
 
-import Exceptions.GrammerMakerError.GrammerUndefined;
-import Exceptions.GrammerMakerError.LeftCommonFactorConflict;
-import Exceptions.GrammerMakerError.TokenisRepeat;
+import Exceptions.GrammerMakerError.GrammerBaseException;
+import Exceptions.GrammerMakerError.Impl.*;
 import GrammarMaker.Reader;
 import Lex.Lex;
 import Parser.Parser;
+import Tree_Span.S;
+import Utils.RunampCompileASTClasses;
+import org.junit.Test;
 
-import javax.tools.JavaCompiler;
-import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
-import javax.tools.ToolProvider;
+import javax.tools.*;
 import java.io.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.List;
 
-import static sun.tools.jconsole.Messages.CLASS_PATH;
+//import static sun.tools.jconsole.Messages.CLASS_PATH;
 
 public class ReaderDemo {
-    public static void main(String[] args) throws IOException, TokenisRepeat, GrammerUndefined, LeftCommonFactorConflict, ClassNotFoundException {
-        Reader reader=new Reader("./");
-        Lex lex=reader.LexGenerate();
-        Parser parser=reader.ParserGenerate();
-        reader.ASTGenerate();
+    @Test
+    public void main() throws IOException, GrammerBaseException, GrammerUndefined, ClassNotFoundException, FollowDebugException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Reader reader=new Reader("D:\\Document\\OneDrive\\CodeRepo\\ASL数据库项目\\ArrayList_Simulate_Tree\\");
+        reader.LexGenerate();
+        reader.ParserGenerate();
+        List<File> javaFiles = reader.ASTGenerate();
+        RunampCompileASTClasses.getInstance().javaCompile(javaFiles.toArray(new File[0]));
+
+        Lex lex=reader.getLex();
+        Parser parser=reader.getParser();
 
         ObjectOutputStream lexStream=new ObjectOutputStream(new FileOutputStream(new File("lex.grammarclass")));
         ObjectOutputStream parserStream=new ObjectOutputStream(new FileOutputStream(new File("parser.grammarclass")));
@@ -47,21 +52,40 @@ public class ReaderDemo {
         }
     }
 
+    @Test
+    public void JavaCompired() throws IOException {
+        Compireing(new File("D:\\Document\\OneDrive\\CodeRepo\\ASL数据库项目\\ArrayList_Simulate_Tree\\src\\main\\java\\Tree_Span\\Impl").listFiles());
+    }
     /**
      * @param files
      * java文件实时编译demo，以后可以据此修改方法
      */
-    private static void javaCompires(File... files) {
+    private static void Compireing(File... files) throws IOException {
         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        StandardJavaFileManager manager = compiler.getStandardFileManager(null, null, null);
+        DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
+        StandardJavaFileManager manager = compiler.getStandardFileManager(diagnostics, null, null);
         Iterable<? extends JavaFileObject> it = manager.getJavaFileObjects(files);
 
         ArrayList<String> ops = new ArrayList<String>();
         ops.add("-classpath");
-        ops.add(CLASS_PATH);
+        StringBuilder classpath=new StringBuilder();
+        classpath.append(java.net.URLDecoder.decode(new S().getClass().getResource("").getFile(),"utf-8"));
+        classpath.delete(classpath.indexOf("Tree_Span/"),classpath.length()-1);
+        classpath.deleteCharAt(0);
+        classpath.insert(0,';');
+        classpath.insert(0,java.net.URLDecoder.decode(new S().getClass().getResource("/").getFile(),"utf-8"));
+        classpath.deleteCharAt(0);
+        ops.add(classpath.toString());
+        ops.add("-sourcepath");
+        ops.add(new S().getClass().getResource("/").getPath().
+                replace("target/test-classes/","src/main/java/"));
 
-        JavaCompiler.CompilationTask task =compiler.getTask(null, manager, null, ops, null, it);
-        task.call();
+        JavaCompiler.CompilationTask task =compiler.getTask(null, manager, diagnostics, ops, null, it);
+
+        if(task.call()) {
+            System.out.println(compiler.name());
+        }
+        manager.close();
     }
 
     /**
